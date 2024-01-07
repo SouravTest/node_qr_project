f9ff7a1217319733c128ed6a85269b05f450cd83
SOURAV <MAIL.MYMAIL11@GMAIL.COM>
2024-01-07 15:59:15 +0530

    middleware add cookie

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -4,24 +4,41 @@ const jwt = require("jsonwebtoken");
 // MIDDLEWARE FOR AUTHORIZATION (MAKING SURE THEY ARE LOGGED IN)
 const isLoggedIn = async (req, res, next) => {
   try {
-    // check if auth header exists
-    if (req.headers.authorization) {
-      // parse token from header
-      const token = req.headers.authorization.split(" ")[1]; //split the header and get the token
-      if (token) {
-        const payload = await jwt.verify(token, process.env.JWT_SECRET);
+    //checking headers or cookie availabe
+    if (req.headers.authorization) {   //if header
+      // check if auth header exists
+      if (req.headers.authorization) {
+        // parse token from header
+        const token = req.headers.authorization.split(" ")[1]; //split the header and get the token
+        if (token) {
+          const payload = await jwt.verify(token, process.env.JWT_SECRET);
+          if (payload) {
+            // store user data in request object
+            req.user = payload;
+            next();
+          } else {
+            res.status(400).json({ error: "token verification failed" });
+          }
+        } else {
+          res.status(400).json({ error: "malformed auth header" });
+        }
+      } else {
+        res.status(400).json({ error: "No authorization header" });
+      }
+    } else {              //if cookie
+      const tokenCookie = req.cookies.token;
+      if (tokenCookie) {
+        const payload = await jwt.verify(tokenCookie, process.env.JWT_SECRET);
         if (payload) {
           // store user data in request object
           req.user = payload;
           next();
         } else {
-          res.status(400).json({ error: "token verification failed" });
+          res.status(400).json({ error: "cookie token verification failed" });
         }
       } else {
-        res.status(400).json({ error: "malformed auth header" });
+        res.status(400).json({ error: "malformed auth token cookie" });
       }
-    } else {
-      res.status(400).json({ error: "No authorization header" });
     }
   } catch (error) {
     res.status(400).json({ error });
